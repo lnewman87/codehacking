10ae0701e0c271b194ad21e73a7e5446058d38f9
lee.newman <lee.newman@users.noreply.github.com>
2018-11-07 21:49:23 +0000

    Create Users form added

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -17,7 +19,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
 
 
     }
@@ -30,7 +34,10 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -42,6 +49,8 @@ class AdminUsersController extends Controller
     public function store(Request $request)
     {
         //
+
+        return $request->all();
     }
 
     /**
