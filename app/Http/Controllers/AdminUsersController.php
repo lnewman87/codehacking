a92d5f2d1621c256c505e3a7186820bdea36d565
Lee Newman <lee-newman@hotmail.co.uk>
2018-11-08 11:42:05 +0000

    Uploading of profile pics added

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -51,11 +52,25 @@ class AdminUsersController extends Controller
     {
         //
 
-        User::create($request->all());
+        $input = $request->all();
 
-        redirect('/admin/users');
+        if($file = $request->file('photo_id')){
 
-        return $request->all();
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+        //redirect('/admin/users');
     }
 
     /**
